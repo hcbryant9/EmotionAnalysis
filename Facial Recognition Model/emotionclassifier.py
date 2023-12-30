077d8f79d8d5d672c4e6bfdd819c8bdad02ba4d8
hcbryant9 <bryanthc@vcu.edu>
2023-12-29 21:22:44 -0500

    adding files

---
+++ Facial Recognition Model/emotionclassifier.py
@@ -0,0 +1,45 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, classification_report
+import joblib
+
+# Step 1: Load the CSV Data
+data = pd.read_csv('sample_data.csv')
+
+# Step 2: Preprocess the Data
+X = data.drop('Emotion', axis=1)  # Features (all landmarks)
+y = data['Emotion']  # Labels (emotions)
+
+# Step 3: Split the Data into Training and Testing Sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+
+# Step 4: Train the Models
+
+# Decision Tree
+dt_classifier = DecisionTreeClassifier()
+dt_classifier.fit(X_train, y_train)
+
+# Save the trained Decision Tree model
+joblib.dump(dt_classifier, 'decision_tree_model.pkl')
+
+# Random Forest
+rf_classifier = RandomForestClassifier()
+rf_classifier.fit(X_train, y_train)
+
+# Save the trained Random Forest model
+joblib.dump(rf_classifier, 'random_forest_model.pkl')
+
+# Step 5: Make Predictions and Evaluate the Models
+
+# Predictions
+dt_predictions = dt_classifier.predict(X_test)
+rf_predictions = rf_classifier.predict(X_test)
+
+# Evaluation
+print("Decision Tree Accuracy:", accuracy_score(y_test, dt_predictions))
+print("Random Forest Accuracy:", accuracy_score(y_test, rf_predictions))
+
+print("\nDecision Tree Classification Report:\n", classification_report(y_test, dt_predictions))
+print("Random Forest Classification Report:\n", classification_report(y_test, rf_predictions))
---
+++ Facial Recognition Model/sample.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import numpy as np
+
+# Generating sample data (random numbers here, replace this with actual data)
+num_samples = 1  # Number of samples
+num_features = 63   # Number of facial landmarks
+
+# Creating random data for each facial landmark position
+data = np.random.rand(num_samples, num_features)
+
+# Creating column names for each facial landmark position
+columns = [
+    "BrowLowererL", "BrowLowererR", "CheekPuffL", "CheekPuffR", "CheekRaiserL", "CheekRaiserR",
+    "CheekSuckL", "CheekSuckR", "ChinRaiserB", "ChinRaiserT", "DimplerL", "DimplerR",
+    "EyesClosedL", "EyesClosedR", "EyesLookDownL", "EyesLookDownR", "EyesLookLeftL", "EyesLookLeftR",
+    "EyesLookRightL", "EyesLookRightR", "EyesLookUpL", "EyesLookUpR", "InnerBrowRaiserL", "InnerBrowRaiserR",
+    "JawDrop", "JawSidewaysLeft", "JawSidewaysRight", "JawThrust", "LidTightenerL", "LidTightenerR",
+    "LipCornerDepressorL", "LipCornerDepressorR", "LipCornerPullerL", "LipCornerPullerR", "LipFunnelerLB",
+    "LipFunnelerLT", "LipFunnelerRB", "LipFunnelerRT", "LipPressorL", "LipPressorR", "LipPuckerL", "LipPuckerR",
+    "LipStretcherL", "LipStretcherR", "LipSuckLB", "LipSuckLT", "LipSuckRB", "LipSuckRT", "LipTightenerL",
+    "LipTightenerR", "LipsToward", "LowerLipDepressorL", "LowerLipDepressorR", "MouthLeft", "MouthRight",
+    "NoseWrinklerL", "NoseWrinklerR", "OuterBrowRaiserL", "OuterBrowRaiserR", "UpperLidRaiserL", "UpperLidRaiserR",
+    "UpperLipRaiserL", "UpperLipRaiserR"
+]
+
+
+# Adding the generated data to a pandas DataFrame
+df = pd.DataFrame(data, columns=columns)
+
+# Adding a sample emotion column with random emotions (for demonstration)
+emotions = ["Happy", "Sad", "Mad", "Anxious"]
+df['Emotion'] = np.random.choice(emotions, num_samples)
+
+# Save the generated data to a CSV file
+df.to_csv('sample_single_emotion.csv', index=False)
