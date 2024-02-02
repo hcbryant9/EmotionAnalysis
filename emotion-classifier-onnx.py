92cef767b461b5a7a526f984ff1f6538ad48c699
hcbryant9 <bryanthc@vcu.edu>
2024-02-01 22:39:34 -0500

    refactoring & adding onnx models instead of pickle

---
+++ emotion-classifier-onnx.py
@@ -0,0 +1,39 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, classification_report
+import joblib
+from skl2onnx import convert_sklearn
+from skl2onnx.common.data_types import FloatTensorType
+
+# Step 1: Load the CSV Data
+data = pd.read_csv('data/unity_collection.csv')
+
+# Step 2: Preprocess the Data
+X = data.drop('Emotion', axis=1)  # Features (all landmarks)
+y = data['Emotion']  # Labels (emotions)
+
+# Step 3: Split the Data into Training and Testing Sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+
+# Step 4: Train the Models
+
+# Decision Tree
+dt_classifier = DecisionTreeClassifier()
+dt_classifier.fit(X_train, y_train)
+
+# Convert and Save the trained Decision Tree model as ONNX
+initial_type = [('float_input', FloatTensorType([None, X_train.shape[1]]))]
+onnx_model = convert_sklearn(dt_classifier, initial_types=initial_type)
+with open("decision_tree_model.onnx", "wb") as f:
+    f.write(onnx_model.SerializeToString())
+
+# Random Forest
+rf_classifier = RandomForestClassifier()
+rf_classifier.fit(X_train, y_train)
+
+# Convert and Save the trained Random Forest model as ONNX
+onnx_model = convert_sklearn(rf_classifier, initial_types=initial_type)
+with open("random_forest_model.onnx", "wb") as f:
+    f.write(onnx_model.SerializeToString())
