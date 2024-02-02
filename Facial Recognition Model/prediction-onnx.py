199213e1fa72cd31b35d3449ca82f3a9bcb67d70
hcbryant9 <bryanthc@vcu.edu>
2024-02-01 22:51:09 -0500

    predicition-onnx.py not yet working

---
+++ Facial Recognition Model/prediction-onnx.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import onnxruntime as ort
+
+# Load ONNX models
+dt_model = ort.InferenceSession('models/decision_tree_model.onnx')
+rf_model = ort.InferenceSession('models/random_forest_model.onnx')
+
+# Example new_data (replace this with your actual new data for prediction)
+# Assuming it's in a format similar to the training data
+new_data = pd.read_csv('data/sample_single_emotion.csv')
+
+# Convert new_data to a numpy array
+new_data_np = new_data.values.astype('float32')
+
+# Make Predictions
+new_predictions_dt = dt_model.run(None, {'float_input': new_data_np})
+new_predictions_rf = rf_model.run(None, {'float_input': new_data_np})
+
+# Print predictions
+print("Decision Tree Predictions:", new_predictions_dt)
+print("Random Forest Predictions:", new_predictions_rf)
